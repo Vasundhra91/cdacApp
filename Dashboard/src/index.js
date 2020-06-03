613516aa6ace67e390266b988bdd2b47756531d7
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-03 22:55:51 +0530

    make it better

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -10,7 +10,9 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 import './Custom.css';
 //import AdminLayout from "layouts/Admin.jsx";
 import Home from "views/login";
+import * as serviceWorker from './serviceWorker';
 //const hist = createBrowserHistory();
 
 ReactDOM.render(<UserContextProvider><Home/></UserContextProvider>,document.getElementById("root")
 );
+serviceWorker.unregister();
