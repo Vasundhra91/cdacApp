b64d7bff14d71a597734bd8f755a4bcf091d8246
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-03 23:31:57 +0530

    w

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom";
 //import { createBrowserHistory } from "history";
 //import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {UserContextProvider} from '../src/views/Logincontext'
-// import "bootstrap/dist/css/bootstrap.css";
-// import "assets/scss/paper-dashboard.scss?v=1.1.0";
-//import "assets/demo/demo.css";
-// import "perfect-scrollbar/css/perfect-scrollbar.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.1.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import './Custom.css';
 //import AdminLayout from "layouts/Admin.jsx";
 import Home from "views/login";
-import * as serviceWorker from './serviceWorker';
 //const hist = createBrowserHistory();
 
 ReactDOM.render(<UserContextProvider><Home/></UserContextProvider>,document.getElementById("root")
 );
-serviceWorker.unregister();
