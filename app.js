631dfb306dcc777e1c489af9616a68c6ae26ed1d
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-03 20:30:06 +0530

    ddd
    dscfds

--- app.js
+++ app.js
@@ -24,15 +24,8 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(express.static(__dirname+'/ConfigFile'));
 controller(app)
 if (process.env.NODE_ENV === 'production') {
   console.log("object+"+ express.static('Dashboard/build'))
@@ -45,5 +38,5 @@ if (process.env.NODE_ENV === 'production') {
    
   });
 }
-app.listen(3001);
+
 module.exports = app;
