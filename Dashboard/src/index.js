b75a33eb194da96e82ba1d27f553da274f5e9a52
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-03 21:52:00 +0530

    make it better

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import {UserContextProvider} from '../src/views/Logincontext'
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.1.0";
-import "assets/demo/demo.css";
+//import "assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 import './Custom.css';
 //import AdminLayout from "layouts/Admin.jsx";
