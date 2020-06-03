57a8e593bcf6b59bab8dfb12f2ff11076d3a0004
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-03 23:20:18 +0530

    make it better

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 //import { createBrowserHistory } from "history";
 //import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {UserContextProvider} from '../src/views/Logincontext'
-import "bootstrap/dist/css/bootstrap.css";
-import "assets/scss/paper-dashboard.scss?v=1.1.0";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "assets/scss/paper-dashboard.scss?v=1.1.0";
 //import "assets/demo/demo.css";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
+// import "perfect-scrollbar/css/perfect-scrollbar.css";
 //import AdminLayout from "layouts/Admin.jsx";
 import Home from "views/login";
 import * as serviceWorker from './serviceWorker';
